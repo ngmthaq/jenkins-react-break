99da710e1a1b1fb1be51a6021fda1050cf9fd8a3
Nguyễn Mạnh Thắng <nguyenmanhthang2000.fb@gmail.com>
2024-12-07 21:19:00 +0700

    feat: add jenkins logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import jenkinsLogo from "./assets/jenkins.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -14,6 +15,9 @@ function App() {
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <a href="https://www.jenkins.io" target="_blank">
+          <img src={jenkinsLogo} className="logo" alt="Jenkins logo" />
         </a>
       </div>
       <h1>Vite + React + Jenkins</h1>
@@ -29,7 +33,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
